dc8e7f82c9caf2165a8c4d27e5474436fc924c44
waillyam23 <waillyam23@users.noreply.github.com>
2018-03-16 16:11:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,63 +1,55 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 
-//compiler sur mac : gcc main.cpp -o main -F/Library/Frameworks -framework SDL2
-
 
 int main(int argc, char **argv)
-{
+{	
     // Notre fenêtre
-    
+	
     SDL_Window* fenetre(0);
     SDL_Event evenements;
     bool terminer(false);
-   
-    
+	
+	
     // Initialisation de la SDL
-    
+	
     if(SDL_Init(SDL_INIT_VIDEO) < 0)
     {
         std::cout << "Erreur lors de l'initialisation de la SDL : " << SDL_GetError() << std::endl;
         SDL_Quit();
-        
+		
         return -1;
     }
-    
-    
+	
+	
     // Création de la fenêtre
-    
+
     fenetre = SDL_CreateWindow("Test SDL 2.0", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
-    
+
     if(fenetre == 0)
     {
         std::cout << "Erreur lors de la creation de la fenetre : " << SDL_GetError() << std::endl;
         SDL_Quit();
-        
+
         return -1;
     }
-    
-    int SDL_RenderDrawLine(SDL_Renderer* renderer,
-                       int           0,
-                       int           300,
-                       int           800,
-                       int           300)
-
-    
+	
+	
     // Boucle principale
-    
+	
     while(!terminer)
     {
-        SDL_WaitEvent(&evenements);
-        
-        if(evenements.window.event == SDL_WINDOWEVENT_CLOSE)
-            terminer = true;
+	SDL_WaitEvent(&evenements);
+		
+	if(evenements.window.event == SDL_WINDOWEVENT_CLOSE)
+	    terminer = true;
     }
-    
-    
+	
+	
     // On quitte la SDL
-    
+	
     SDL_DestroyWindow(fenetre);
     SDL_Quit();
-    
+	
     return 0;
 }
