e501c6bec79caf00fbb59577bf2618464498837a
Oddball74 <37216267+Oddball74@users.noreply.github.com>
2018-03-16 14:08:44 +0100

    Update entities.cpp

--- entities.cpp
+++ entities.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 //Hitbox
-struct Hitbox{
+struct hitbox{
     float x, y, w, h; //position des 4 coordonnées d'une hitbox rectangulaire
 };
 
@@ -12,10 +12,11 @@ struct Player{
     float x;    //position x du joueur
     float y;    //position y du joueur
     float a;    //angle du curseur
-    Hitbox hb;  //hitbox du joueur
+    hitbox h;//hitbox du joueur
     float mspeed;//vitesse du joueur
     float bspeed;//vitesse des projectiles
     float reload;//temps de rechargement
+    
 };
 
 //Projectiles
@@ -24,6 +25,7 @@ struct Bullet{
     float y;    //position y
     float v;    //vitesse de
     float a;    //angle v
+    hitbox h;
 };
 
 //Ennemis
@@ -32,12 +34,18 @@ struct Tank{
     float x;    //position x de l'ennemi
     float y;    //position y de l'ennemi
     float a;    //angle de tir de l'ennemi
-    Hitbox hb;  //hitbox de l'ennemi
+    Hitbox h;  //hitbox de l'ennemi
 };
 
 //Bâtiments
 struct Bat{
     int hp;       //vie du bêtiment
     float x, y;   //position x et y
-    Hitbox hb;    //hitbox du batiment
+    Hitbox h;    //hitbox du batiment
 };
+
+void initPayer (Player*)
+{
+    (*Player).score=0;
+    (*Player).
+}
