c84b298b568dfe505072e547d80f835b1a2b34d8
Oddball74 <37216267+Oddball74@users.noreply.github.com>
2018-03-22 18:11:54 +0100

    Add files via upload

---
+++ Fenetre.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <SDL.h>
+using namespace std;
+int main( int argc,char *argv[])
+{
+
+// creation de la fenetre
+
+  SDL_Init(SDL_INIT_EVERYTHING);
+  SDL_Texture *surf, *ecran=NULL;
+    32 ,100, 100, 100, 0;
+  SDL_Renderer* renderer;
+  SDL_Surface* Loading_Surf;
+
+  SDL_Window *window;
+  window = SDL_CreateWindow("TeamTanks", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1500,800,SDL_WINDOW_RESIZABLE);
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  if(window==NULL)
+    {
+      cout<<"BRUH";
+      }
+
+
+      //tous les test vont ici
+
+
+
+//mise a jour
+  SDL_Event event;
+  bool running=true;
+  while(running)
+    {
+    {//endroit ou on code tout
+
+
+
+
+
+
+    }
+      while(SDL_PollEvent(&event))
+	{
+	  if(event.type==SDL_QUIT)
+	  {
+	    running=false;
+	    break;
+	  }
+      }
+    }
+//fermeture de la fenètre
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
+}
