817a1b0015d82c2ca0f04cff503d82b4eb1e135c
Oddball74 <37216267+Oddball74@users.noreply.github.com>
2018-03-23 14:40:17 +0100

    Update Fenetre.cpp

--- Fenetre.cpp
+++ Fenetre.cpp
@@ -7,15 +7,23 @@ int main( int argc,char *argv[])
 // creation de la fenetre
 
   SDL_Init(SDL_INIT_EVERYTHING);
-  SDL_Texture *texture, *ecran=NULL;
+//initialisation des variables utiles
+SDL_Rect destR;
+destR.x= 0;
+destR.y=500;
+destR.w=40;
+destR.h=35;
 
-  SDL_Renderer* renderer;
-  SDL_Surface* surf;
-  SDL_Texture* BlueShapes;
 
-  SDL_Window *window;
+  SDL_Renderer* renderer; //l'oultil de rendu
+  SDL_Surface* surf;//la surface possedant l'image
+
+  SDL_Window *window;//la fenètre
+//lancement de la fenètre
   window = SDL_CreateWindow("Tank_Crusade", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1500,800,SDL_WINDOW_RESIZABLE);
+//initialisation du rendu et association avec la fenètre
   renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	//test si la fenetre est créée
   if(window==NULL)
     {
       cout<<"BRUH";
@@ -23,12 +31,10 @@ int main( int argc,char *argv[])
 
 
       //tous les test vont ici
-  surf = SDL_LoadBMP("sdl_icone.bmp");
-  BlueShapes = SDL_CreateTextureFromSurface(renderer, surf);
+  surf = SDL_LoadBMP("sdl_icone.bmp");//charge l'image dans la surface
 
-      SDL_RenderPresent(renderer);
-      SDL_BlitSurface(surf, NULL, SDL_GetWindowSurface(window),NULL);
-  //SDL_FreeSurface(surf);
+      SDL_RenderPresent(renderer);//?
+
 
 //mise a jour
   SDL_Event event;
@@ -36,12 +42,13 @@ int main( int argc,char *argv[])
   while(running)
     {
     {//endroit ou on code tout
+destR.x=destR.x+1;
+if(destR.x>1400)
+destR.x=100;
 
-
-SDL_UpdateWindowSurface(window);
-
-
-
+  SDL_BlitSurface(surf, NULL, SDL_GetWindowSurface(window),&destR);//Blite la surface sur le rendu de la fenetre
+SDL_UpdateWindowSurface(window);//rafraichit le rendu dans la fenètre
+SDL_Delay(5);
 
 
     }
@@ -60,3 +67,4 @@ SDL_UpdateWindowSurface(window);
 
   return 0;
 }
+
