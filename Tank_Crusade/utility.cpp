cd6ad362c89d0a6fc9cf1d465a93ee2e88423dae
waillyam23 <waillyam23@users.noreply.github.com>
2018-03-26 19:39:17 +0200

    Add files via upload (#1)

---
+++ Tank_Crusade/utility.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include "entities.cpp"
+
+//Perte de hp du joueur
+void hpLossPlayer(Player joueur){
+        joueur.hp-=1;
+}
+
+//Perte de hp des tanks ennemis
+void hpLossTank(Tank ennemi){
+    ennemi.hp-=1;
+    if (ennemi.hp<1)
+        delete enemi;
+}
