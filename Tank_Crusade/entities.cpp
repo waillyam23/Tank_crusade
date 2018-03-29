a2f53dbbce0f878bdb900ae75b32619eb98b7264
Oddball74 <37216267+Oddball74@users.noreply.github.com>
2018-03-29 17:39:34 +0200

    Update entities.cpp

--- Tank_Crusade/entities.cpp
+++ Tank_Crusade/entities.cpp
@@ -4,6 +4,55 @@
 #include <iostream>
 
 #include "entities.h"
+#ifndef __ENTITIES_H__   // si entities.h n'a pas encore été ajouté
+#define __ENTITIES_H__   // alord dire au compilateur qu'il vient d'être ajouté
+
+#include "SDL2/SDL.h"
+
+//STRUCTURES DES ENTITEES
+
+
+//Joueur
+struct player{
+    int hp;     //vie du joueur
+    int score;  //score du joueur
+    float a;    //angle du curseur
+    SDL_Rect c; //coordonnées et hitbox du joueur
+    int mspeed;//vitesse du joueur
+    float bspeed;//vitesse des projectiles
+    float reload;//temps de rechargement
+    float munition;//quelle munition ?
+    float canons;//quel canon ?
+    float elevation;//elevation maximale du canon
+    float depression;//elevation minimale du canon
+
+};
+
+//Projectiles
+struct bullet{
+    SDL_Rect c; //coordonnées et hitbox du projectile
+    float v;    //vitesse de
+    float a;    //angle v
+};
+
+//Ennemis
+struct tank{
+    int hp;     //vie de l'ennemi
+    SDL_Rect c; //coordonnées et hitbox du tank
+    float a;    //angle de tir de l'ennemi
+};
+
+//Bâtiments
+struct bat{
+    int hp;       //vie du bêtiment
+    SDL_Rect c; //coordonnées et hitbox du batiment
+};
+
+//PROTOTYPES DES FONCTIONS
+
+void initPlayer (player* P1);
+
+#endif
 
 void initPlayer (player* P1)
 {
