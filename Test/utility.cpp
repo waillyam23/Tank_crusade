097a30bb118ea15eed897b749fb1ba34b8b4f3a8
Oddball74 <37216267+Oddball74@users.noreply.github.com>
2018-04-11 15:55:00 +0200

    Update utility.cpp

--- Test/utility.cpp
+++ Test/utility.cpp
@@ -99,6 +99,42 @@ void tir(bullet* B)
     (*B).c.y = (*B).c.y+(*B).a;
 }
 
+bool colliProjEnnemi (bullet* box1, tank* box2)
+{
+  if(((*box2).c.x >= (*box1).c.x + (*box1).c.w)      // trop à droite
+     || ((*box2).c.x + (*box2).c.w <= (*box1).c.x) // trop à gauche
+     || ((*box2).c.y >= (*box1).c.y + (*box1).c.h) // trop en bas
+     || ((*box2).c.y + (*box2).c.h <= (*box1).c.y))  // trop en haut
+          return false;
+ else
+          return true;
+}
+
+//FONCTION collision entre projectile tank et joueur
+bool colliProjJoueur (bullet* box1, player* box2)
+{
+  if(((*box2).c.x >= (*box1).c.x + (*box1).c.w)      // trop à droite
+     || ((*box2).c.x + (*box2).c.w <= (*box1).c.x) // trop à gauche
+     || ((*box2).c.y >= (*box1).c.y + (*box1).c.h) // trop en bas
+     || ((*box2).c.y + (*box2).c.h <= (*box1).c.y))  // trop en haut
+          return false;
+  else
+          return true;
+}
+
+//FONCTION collision entre projectile et batiment
+bool colliProjBatiment (bullet* box1, bat* box2)
+{
+  if(((*box2).c.x >= (*box1).c.x + (*box1).c.w)      // trop à droite
+     || ((*box2).c.x + (*box2).c.w <= (*box1).c.x) // trop à gauche
+     || ((*box2).c.y >= (*box1).c.y + (*box1).c.h) // trop en bas
+     || ((*box2).c.y + (*box2).c.h <= (*box1).c.y))  // trop en haut
+          return false;
+  else
+          return true;
+}
+
+
 /*void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination, SDL_Rect* clip = NULL )
 {
     SDL_Rect offset;
